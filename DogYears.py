da4bcc4aba04934f7f8f6c9bb858161c1f13b393
diketj0753 <47456333+diketj0753@users.noreply.github.com>
2019-02-16 09:09:38 -0500

    Gives age in dog years

---
+++ DogYears.py
@@ -0,0 +1,15 @@
+# Give the user their age in dog years.
+
+# Ask user for their name.
+name=input('Hello, what is your name? ')
+
+# Ask user for their age.
+Age_Years=int(input(name+', how old are you? '))
+
+# Convert age to dog years by multiplying by 7.
+Age_Dog_Years=Age_Years*7
+
+# Tell the user their age in dog years.
+print('Wow!  You\'re', Age_Dog_Years, 'years old in dog years,', name)
+
+input()
